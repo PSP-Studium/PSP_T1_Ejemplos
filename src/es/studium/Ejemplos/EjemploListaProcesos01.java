694bcdd5f778edaf9909b146d4e2f6659a21b4e9
Alvaro Carballo <alv.carballo@outlook.es>
2021-01-29 00:39:40 +0100

    Final

---
+++ src/es/studium/Ejemplos/EjemploListaProcesos01.java
@@ -0,0 +1,22 @@
+package es.studium.Ejemplos;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class EjemploListaProcesos01 {
+	public static void main(String[] args)
+	{
+	try {
+	    String line;
+	    Process p = Runtime.getRuntime().exec("tasklist.exe");
+	    BufferedReader input =
+	            new BufferedReader(new InputStreamReader(p.getInputStream()));
+	    while ((line = input.readLine()) != null) {
+	        System.out.println(line); //<-- Parse data here.
+	    }
+	    input.close();
+	} catch (Exception err) {
+	    err.printStackTrace();
+	}
+	}
+}
